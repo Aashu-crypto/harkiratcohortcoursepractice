a311e06a5804707961198c493f4b844e894d249f
Aashu-crypto <ashugandotra14@gmail.com>
2024-01-05 17:16:58 +0530

    express basix

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello world")
-})
+const users = [
+  {
+    name: "Johnny",
+    kidneys: [
+      {
+        healthy: false,
+      },
+      
+    ],
+  },
+];
 
-app.listen(port,()=>{
-    console.log('Server Started');
-})
+app.get("/", (req, res) => {
+  const johnkidney = users[0].kidneys;
+  const noOfKidneys = johnkidney.length;
+  let noOfHealthyKidney = 0;
+  for (let i = 0; i < noOfKidneys; i++) {
+    if (johnkidney[i].healthy) {
+      noOfHealthyKidney = noOfHealthyKidney+1;
+    }
+  }
+  let noOfUnhealthyKidney = noOfKidneys - noOfHealthyKidney;
+  res.json({ noOfKidneys, noOfHealthyKidney, noOfUnhealthyKidney });
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page Hai sir ya");
+});
+
+app.get("/setting", (req, res) => {
+  res.send("setting Page h ya sir");
+});
+
+app.listen(port, () => {
+  console.log("Server Started");
+});
