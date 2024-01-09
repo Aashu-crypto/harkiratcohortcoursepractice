f248d39cb3e17a6c503cf995f9124b3ed8ad3dbe
Aashu-crypto <ashugandotra14@gmail.com>
2024-01-09 16:22:09 +0530

    done with post,put

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+app.use(express.json())
 const users = [
   {
     name: "Johnny",
@@ -9,7 +9,6 @@ const users = [
       {
         healthy: false,
       },
-      
     ],
   },
 ];
@@ -20,13 +19,32 @@ app.get("/", (req, res) => {
   let noOfHealthyKidney = 0;
   for (let i = 0; i < noOfKidneys; i++) {
     if (johnkidney[i].healthy) {
-      noOfHealthyKidney = noOfHealthyKidney+1;
+      noOfHealthyKidney = noOfHealthyKidney + 1;
     }
   }
   let noOfUnhealthyKidney = noOfKidneys - noOfHealthyKidney;
   res.json({ noOfKidneys, noOfHealthyKidney, noOfUnhealthyKidney });
 });
 
+
+app.post('/',(req,res)=>{
+  const isHealthy =req.body.isHealthy
+  users[0].kidneys.push({
+    healthy:isHealthy
+  })
+  res.json({
+    msg:"Done!"
+  })
+})
+
+app.put('/',(req,res)=>{
+  for(let i = 0; i<users[0].kidneys.length;i++){
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json({
+    "Good":"Word"
+  })
+})
 app.get("/about", (req, res) => {
   res.send("About Page Hai sir ya");
 });
